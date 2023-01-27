dce74d2c2b6a7a79a227192f2c04d38ee82bb9b9
Fonkeyy <113917387+Fonkeyy@users.noreply.github.com>
2023-01-27 08:27:53 +0100

    up

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,75 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    let filteredArr = [];
+    arr.forEach((element) => {
+        if (!toRemove.includes(element)) {
+            filteredArr.push(element);
+        }
+    });
+    return filteredArr;
 };
+  
+
+
+
+/*     return filteredArr.concat(arr.filter(j => j !== toRemove));
+}   */
+   
+   
+   
+   
+    /*for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] == toRemove) {
+            arr.splice(i, 1);
+        } else if (toRemove.length > 1) {
+            for (let j = 1; j <= toRemove.length; i++) {
+                if (arr[i] == toRemove[j]) {
+                    arr.splice(i, 1);
+                }
+            }
+        } else return arr;
+    }
+    return arr;
+};*/
+   
+   
+   
+   
+   
+   
+    
+
+   
+   
+     /*for (let i = 0;  i <=arr.length; i++) {
+        if (arr[i] == ()) {
+            arr[i].shift;
+        }
+        
+    }
+    return arr;
+}*/
+  
+  
+  
+  
+    /* for (let i = 0; i <= arr.length; i++) {
+        if (toRemove.length > 1) {
+            for (let j = 0; j <= toRemove.length; i++) {
+                if (arr[i] == toRemove[j]) {
+                    arr.splice(i, 1);
+                }
+            }
+        } else if (arr[i] == toRemove) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
+};*/
+    
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*if (arr[i] == [...toRemove]) {
+    arr.splice(i, 1);*/
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(startArg, finishArg) {
+arr = [];
+if (((startArg  || finishArg) < 0) || (!Number.isInteger(startArg) || !Number.isInteger(finishArg))) {
+    return 'ERROR';
+} else if (startArg > finishArg) {
+    const temp = startArg;
+    startArg = finishArg;
+    finishArg = temp;
+    for (let i = startArg; i <= finishArg; i++) {
+        arr.push(i);
+  }
+ } else {
+    for (let i = startArg; i <= finishArg; i++) {
+        arr.push(i);
+};
+}
+return arr.reduce((sum, item) => sum + item); 
 };
 
 // Do not edit below this line
